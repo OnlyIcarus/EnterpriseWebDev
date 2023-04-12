f6dfa52d75449f05d43597697ef5759734c4dc3d
Icarus <afterdarkgaming2@gmail.com>
2023-04-13 00:28:00 +0100

    Update App.js

--- enterprise_web_app/src/App.js
+++ enterprise_web_app/src/App.js
@@ -23,7 +23,7 @@ export default function App() {
 
     fetch("http://127.0.0.1:8000/api/users",
     {
-      headers: {'Content-Type': 'application/json'},
+      headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'http://127.0.0.1:8000'},
       method: "POST",
       body: data
     })
