a1cf4e3875d8eb5e742c88385520d5c5d782fcb1
Icarus <afterdarkgaming2@gmail.com>
2023-04-12 18:32:03 +0100

    Update App.js

--- enterprise_web_app/src/App.js
+++ enterprise_web_app/src/App.js
@@ -6,7 +6,18 @@ import './App.css'
 
 export default function App() {
 
+  var path = require('path');
+  var express = require('express');
+  var app = express();
+  const PORT = 8080
 
+  app.use(function ( req, res, next) {
+    res.send('This page does not exist!')
+  });
+
+  app.listen(PORT, function () {
+    console.log('Listening on http://localhost:'+PORT+'/');
+  });
 
   const [todos, setTodos] = useState([])
   const usernameRef = useRef()
