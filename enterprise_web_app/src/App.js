4f30a817ed9ef76f1acf1989f83081f727aec6ea
Icarus <afterdarkgaming2@gmail.com>
2023-04-12 14:34:03 +0100

    fix

--- enterprise_web_app/src/App.js
+++ enterprise_web_app/src/App.js
@@ -10,39 +10,38 @@ export default function App() {
   const usernameRef = useRef()
   const passwordRef = useRef()
 
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos(storedTodos)
-  }, [])
+  // useEffect(() => {
+  //   const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  //   if (storedTodos) setTodos(storedTodos)
+  // }, [])
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
+  // useEffect(() => {
+  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  // }, [todos])
 
-  function toggleTodo(id) {
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
-  }
+  // function toggleTodo(id) {
+  //   const newTodos = [...todos]
+  //   const todo = newTodos.find(todo => todo.id === id)
+  //   todo.complete = !todo.complete
+  //   setTodos(newTodos)
+  // }
 
-  function handleAddTodo(e) {
-    const name = todoNameRef.current.value
-    if (name === '') return
-    setTodos(prevTodos => {
-      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
-    })
-    todoNameRef.current.value = null
-  }
+  // function handleAddTodo(e) {
+  //   const name = todoNameRef.current.value
+  //   if (name === '') return
+  //   setTodos(prevTodos => {
+  //     return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+  //   })
+  //   todoNameRef.current.value = null
+  // }
 
-  function handleClearTodos() {
-    const newTodos = todos.filter(todo => !todo.complete)
-    setTodos(newTodos)
-  }
+  // function handleClearTodos() {
+  //   const newTodos = todos.filter(todo => !todo.complete)
+  //   setTodos(newTodos)
+  // }
 
   return (
     <div id="login-container">
-      <TodoList todos = {todos} toggleTodo = {toggleTodo}/>
       <div id="login-input">
         <input ref={usernameRef} type="text" />
         <input ref={passwordRef} type="text" />
