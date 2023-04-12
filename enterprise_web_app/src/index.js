93b5947493ed25e64b81aaae6f6015dbc581f238
Icarus <afterdarkgaming2@gmail.com>
2023-04-12 19:11:23 +0100

    Update index.js

--- enterprise_web_app/src/index.js
+++ enterprise_web_app/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// const { MongoClient } = require("mongodb");
-// const url = "mongodb://127.0.0.1:27017";
+const { MongoClient } = require("mongodb");
+const url = "mongodb://127.0.0.1:27017";
 
 // const client = new MongoClient(url);
 // async function run() {
