f5df1697f1a41f41e60748e578dbed7fd736f504
Icarus <afterdarkgaming2@gmail.com>
2023-04-12 22:58:50 +0100

    testing login

--- enterprise_web_app/backend/server.js
+++ enterprise_web_app/backend/server.js
@@ -1,7 +1,14 @@
-var path = require('path');
-var express = require('express');
+import path from 'path'
+import express from 'express'
+import userRoutes from './user.routes.js'
+import bodyParser from 'body-parser'
 var app = express();
-const PORT = 8000
+import config from './config'
+const PORT = config.port;
+const MONGOURL = config.mongoUri;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(function (req, res, next) {
   res.send('This page does not exist!')
@@ -12,3 +19,9 @@ app.listen(PORT, function() {
 });
 
 const MONGODB_URL = "mongodb://127.0.0.1:27017";
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise
+mongoose.connect(MONGODB_URL, { dbName: "users" })
+mongoose.connection.on('error', err=> {
+  throw new Error('unable to connect to database: ${MONGODB_URL}')
+})
