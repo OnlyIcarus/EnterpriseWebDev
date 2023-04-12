4f00c12f675968440486234be967bba685288b96
Icarus <afterdarkgaming2@gmail.com>
2023-04-12 20:39:58 +0100

    Update server.js

--- enterprise_web_app/backend/server.js
+++ enterprise_web_app/backend/server.js
@@ -1,23 +1,12 @@
-const { MongoClient } = require("mongodb");
-const url = "mongodb://127.0.0.1:27017";
+var path = require('path');
+var express = require('express');
+var app = express();
+const PORT = 8000
 
-// Database stuff
-// Create a new MongoClient
-const client = new MongoClient(url);
-async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-    console.log('Start the database stuff');
-    //Write databse Insert/Update/Query code here..
-    console.log('End the database stuff');
+app.use(function (req, res, next) {
+  res.send('This page does not exist!')
+});
 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+app.listen(PORT, function() {
+  console.log("Listening on http://localhost"+PORT+"/")
+});
