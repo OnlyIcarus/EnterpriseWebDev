0633757784380adb0809c36803dc923700d2cec1
Icarus <afterdarkgaming2@gmail.com>
2023-04-13 00:35:41 +0100

    Update server.js

--- enterprise_web_app/backend/server.js
+++ enterprise_web_app/backend/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', userRoutes)
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
