1a0646c20f17037aa023b5b44adf55a23b1a51a2
Icarus <afterdarkgaming2@gmail.com>
2023-04-12 23:15:37 +0100

    fix

--- enterprise_web_app/backend/server.js
+++ enterprise_web_app/backend/server.js
@@ -1,29 +1,30 @@
-import path from 'path'
-import express from 'express'
+import path from 'path';
+import express from 'express';
 import userRoutes from './user.routes.js'
 import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+
 var app = express();
-import config from './config.js'
+
+import config from './config.js';
 const PORT = config.port;
-const MONGOURL = config.mongoUri;
+const MONGOURI = config.mongoUri;
 
+await mongoose.connect('mongodb://127.0.0.1/my_database');
+
+
+// parse body params and attache them to req.body
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-
-app.use(function (req, res, next) {
-  res.send('This page does not exist!')
-});
-
-app.listen(PORT, function() {
-  console.log("Listening on http://localhost"+PORT+"/")
-});
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', userRoutes)
 
-const MONGODB_URL = "mongodb://127.0.0.1:27017";
-mongoose.Promise = global.Promise
-mongoose.connect(MONGODB_URL, { dbName: "users" })
-mongoose.connection.on('error', err=> {
-  throw new Error('unable to connect to database: ${MONGODB_URL}')
-})
+// 404 page
+app.use(function ( req, res, next) {
+    res.send('This page does not exist!')
+});
+
+app.listen(PORT, function () {
+    console.log('Listening on http://localhost:'+PORT+'/');
+});
+
