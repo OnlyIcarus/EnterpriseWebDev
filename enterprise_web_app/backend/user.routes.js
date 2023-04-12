6ef615d845f40c8bfafe3ea32957f1bbbd06ba39
Icarus <afterdarkgaming2@gmail.com>
2023-04-12 23:39:21 +0100

    fix?

--- enterprise_web_app/backend/user.routes.js
+++ enterprise_web_app/backend/user.routes.js
@@ -3,15 +3,21 @@ import userCtrl from './user.controller.js'
 
 const router = express.Router()
 
-router.route('api/users')
+router.route('/api/users')
   .get(userCtrl.list)
   .post(userCtrl.create)
 
-router.route('api/users/:userId')
+router.route('/api/users/:userId')
   .get(userCtrl.read)
   .put(userCtrl.update)
   .delete(userCtrl.remove)
 
-router.param('userId', userCtrl.userByID)
+router.route('/api/userbyname/:userName')
+  .get(userCtrl.read)
+  .put(userCtrl.update)
+  .delete(userCtrl.remove)
 
+  router.param('userId', userCtrl.userByID)
+  router.param('userName', userCtrl.userByName)
+  
 export default router
