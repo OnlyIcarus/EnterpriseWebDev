0cd95c5f241b9e9ed750845c6be58233b06c8a07
Icarus <afterdarkgaming2@gmail.com>
2023-04-12 23:00:41 +0100

    f

--- enterprise_web_app/src/App.js
+++ enterprise_web_app/src/App.js
@@ -2,10 +2,7 @@ import React, { useState, useRef, useEffect } from 'react'
 import {v4 as uuidv4} from 'uuid';
 import './App.css'
 
-//const LOCAL_STORAGE_KEY = 'todoApp.todos'
-
 export default function App() {
-
   const [todos, setTodos] = useState([])
   const usernameRef = useRef()
   const passwordRef = useRef()
