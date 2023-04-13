2c59c207ac8b62d364f0f3ba6015f4fa08234c2e
Icarus <afterdarkgaming2@gmail.com>
2023-04-13 04:37:08 +0100

    moving on from login

--- enterprise_web_app/src/App.js
+++ enterprise_web_app/src/App.js
@@ -46,6 +46,7 @@ export default function App() {
   function handleLogin() {
     const email = loginEmailRef.current.value;
     const password = loginPassRef.current.value;
+    const login = false;
 
     fetch("auth/signin",
     {
@@ -54,35 +55,43 @@ export default function App() {
       body: JSON.stringify( { email: email, password: password} )
     }).then(function (response) {
       if (response.status === 401) {
-        alert ("failure");
+        alert ("Wrong email and password combination");
       } else {
-        alert("success");
+        alert("You have logged in succesfully");
+        login = true;
       }
     })
+
+    if (login) {
+      getElementById('register-container').style.display = "none";
+      getElementById('login-container').style.display = "none";
+    }
   }
 
   return (
-    <div id="login-page">
-      <div id="login-button-container">
-        <div id="register-button" onClick={switchPageLogin}>Already a User? Login Here</div>
-      </div>
-      <div id="register-container">
-        <div id="register-input">
-          <input ref={usernameRef} type="text" placeholder="Username..." className="inputLogin" />
-          <br />
-          <input ref={passwordRef} type="password" placeholder="Password..." className="inputLogin" />
-          <br />
-          <input ref={emailRef} type="text" placeholder="E-Mail..." className="inputLogin" />
+    <div id="container">
+      <div id="login-page">
+        <div id="login-button-container">
+          <div id="register-button" onClick={switchPageLogin}>Already a User? Login Here</div>
         </div>
-        <button id="register-button" onClick={handleRegister}>Register</button>
-      </div>
-      <div id="login-container">
-        <div id="login-input">
-          <input ref={loginEmailRef} type="text" placeholder="Email..." className="inputLogin" />
-          <br />
-          <input ref={loginPassRef} type="password" placeholder="Password..." className="inputLogin" />
+        <div id="register-container">
+          <div id="register-input">
+            <input ref={usernameRef} type="text" placeholder="Username..." className="inputLogin" />
+            <br />
+            <input ref={passwordRef} type="password" placeholder="Password..." className="inputLogin" />
+            <br />
+            <input ref={emailRef} type="text" placeholder="E-Mail..." className="inputLogin" />
+          </div>
+          <button id="register-button" onClick={handleRegister}>Register</button>
         </div>
-        <button id="register-button" onClick={handleLogin}>Log In</button>
+        <div id="login-container">
+          <div id="login-input">
+            <input ref={loginEmailRef} type="text" placeholder="Email..." className="inputLogin" />
+            <br />
+            <input ref={loginPassRef} type="password" placeholder="Password..." className="inputLogin" />
+          </div>
+          <button id="register-button" onClick={handleLogin}>Log In</button>
+        </div>
       </div>
     </div>
   )
