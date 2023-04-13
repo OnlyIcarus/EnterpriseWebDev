900063a9bda9b5b5428b30fc8cb536528839bf5a
Icarus <afterdarkgaming2@gmail.com>
2023-04-13 04:18:02 +0100

    security updates

--- enterprise_web_app/backend/auth.controller.js
+++ enterprise_web_app/backend/auth.controller.js
@@ -8,46 +8,47 @@ const signin = async (req, res) => {
     let user = await User.findOne({
       "email": req.body.email
     })
-    if (!user) {
+    if (!user)
       return res.status(401).json({
         error: "User not found"
       })
 
-      if (!user.authenticate(req.body.password)) {
-        return res.status(401).send({
-          error: "Email and password don't match"
-        })
-      }
-
-      const token = jwt.sign({
-        _id: user._id
-      }, config.jwtSecret)
-
-      res.cookie("t", token, {
-        expire: new Date() + 9999
-      })
-
-      return res.json({
-        token,
-        user: {
-          _id: user._id,
-          name: user.name,
-          email: user.email
-        }
+    if (!user.authenticate(req.body.password)) {
+      return res.status(401).send({
+        error: "Email and password don't match."
       })
     }
+
+    const token = jwt.sign({
+      _id: user._id
+    }, config.jwtSecret)
+
+    res.cookie("t", token, {
+      expire: new Date() + 9999
+    })
+
+    return res.json({
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    })
+
   } catch (err) {
-    return res.status(401).json ({
+    return res.status(401).json({
       error: "Could not sign in"
     })
   }
+
 }
 
 const signout = (req, res) => {
   res.clearCookie("t")
   return res.status(200).json({
     message: "signed out"
-  })
+  })  
 }
 
 const requireSignin = expressJwt({
@@ -57,7 +58,7 @@ const requireSignin = expressJwt({
 })
 
 const hasAuthorization = (req, res, next) => {
-  const authorized = req.profile && req.auth && req.profile_id == req.auth._id
+  const authorized = req.profile && req.auth && req.profile._id == req.auth._id
   if (!(authorized)) {
     return res.status(403).json({
       error: "User is not authorized"
@@ -65,6 +66,7 @@ const hasAuthorization = (req, res, next) => {
   }
   next()
 }
+
 
 export default {
   signin,
