5f66d4ee543f85b58fd2247a875de8ee8216c386
Icarus <afterdarkgaming2@gmail.com>
2023-04-14 09:55:36 +0100

    Update quote.model.js

--- enterprise_web_app/backend/quote.model.js
+++ enterprise_web_app/backend/quote.model.js
@@ -3,46 +3,46 @@ import crypto from 'crypto'
 
 const UserSchema = new mongoose.Schema({
   casual_workers: {
-    type: String,
+    type: Number,
     trim: true,
     required: 'Casual Workers is required'
   },
   casual_worker_pay: {
-    type: Integer,
+    type: Number,
     required: 'Casual Worker Pay is required'
   },
   average_casual_hours: {
-    type: Integer,
+    type: Number,
     required: 'Average hours is required'
   },
   standard_workers: {
-    type: String,
+    type: Number,
     trim: true,
     required: 'Standard Workers is required'
   },
   standard_worker_pay: {
-    type: Integer,
+    type: Number,
     required: 'Standard Worker Pay is required'
   },
   average_standard_hours: {
-    type: Integer,
+    type: Number,
     required: 'Average hours is required'
   },
   expert_workers: {
-    type: String,
+    type: Number,
     trim: true,
     required: 'Expert Workers is required'
   },
   expert_worker_pay: {
-    type: Integer,
+    type: Number,
     required: 'Expert Worker Pay is required'
   },
   average_expert_hours: {
-    type: Integer,
+    type: Number,
     required: 'Average hours is required'
   },
   total_physical: {
-    type: Integer,
+    type: Number,
     required: 'Physical item is not required'
   },
 
